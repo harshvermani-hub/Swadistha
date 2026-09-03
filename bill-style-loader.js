75bc6a72fbe2c0b919090166012036c10dba9e51
harshvermani-hub <harsh.vermani@gmail.com>
2026-09-03 21:34:45 +0530

    Add Bill Style loader

---
+++ bill-style-loader.js
@@ -0,0 +1,7 @@
+/* Loads the Bill Style customization UI. */
+(function(){
+  const s=document.createElement('script');
+  s.src='bill-style-customization.js';
+  s.defer=true;
+  document.head.appendChild(s);
+})();
